d4bbde616023aba32df888c9b6e22fe528f2f277
Jeeban jyoti Dhal <jeebanjyoti413@gmail.com>
2021-03-22 22:06:50 +0530

    changes

--- src/main/java/in/ashokit/controllers/StudentController.java
+++ src/main/java/in/ashokit/controllers/StudentController.java
@@ -7,9 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import in.ashokit.dao.StudentDao;
 import in.ashokit.entity.Student;
 import in.ashokit.service.StudentService;
 
